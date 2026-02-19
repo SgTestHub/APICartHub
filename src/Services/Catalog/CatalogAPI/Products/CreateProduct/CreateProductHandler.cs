c5e42a651218d4e36058e8c1590b0fc9c85d0940
Sheetal <gajare.sheetal@gmail.com>
2026-02-19 22:17:25 +1100

    handler

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,19 @@
-﻿namespace CatalogAPI.Products.CreateProduct
+﻿using MediatR;
+namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductCommand(
         string Name,
         string Description,
         decimal Price
-    );
+    ) : IRequest<CreateProductResult>;
 
     public record CreateProductResult( Guid Id );
-    internal class CreateProductCommandHandler
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
-
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business logic to create a product would go here, such as validating the input, saving the product to a database, etc.
+            throw new NotImplementedException();
+        }
     }
 }
